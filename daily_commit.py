da2dddc7d5841be5e990d4ff8010275a87380807
Justin McLain <justinkeithmclain@gmail.com>
2026-04-24 11:28:15 -0500

    Add Error Handling to Python Script

--- daily_commit.py
+++ daily_commit.py
@@ -1,16 +1,27 @@
 import os
 import datetime
 import subprocess
+import traceback
+import sys
 
 repo_path = os.path.dirname(os.path.abspath(__file__))
-
 os.chdir(repo_path)
 
-file_name = 'daily_commit.txt'
-with open(file_name, 'a') as f:
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    f.write(f'{today}\n')
+try:
+    file_name = "daily_commit.txt"
 
-subprocess.run(['git', 'add', '.'], check=True)
-subprocess.run(['git', 'commit', '-m', 'Daily commit'], check=True)
-subprocess.run(['git', 'push', 'origin', 'main'], check=True)
+    with open(file_name, "a") as f:
+        today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"{today}\n")
+
+    git = "/usr/bin/git"
+
+    subprocess.run([git, "add", "."], check=True)
+    subprocess.run([git, "commit", "-m", "Daily commit"], check=True)
+    subprocess.run([git, "push", "origin", "main"], check=True)
+
+    print("Success")
+
+except Exception:
+    traceback.print_exc()
+    sys.exit(1)
